51a0e86780cab8df58908c4290238e1c18d73586
Your Name <amitpatni13@gmail.com>
2023-07-01 05:09:13 -0400

    final project task 1, 2 completed

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { expenses, budget } = useContext(AppContext);
+    const [budgetAmt, setBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
+    const OnBudgetChange = (event) => {
+        const budgetVal = event.target.value;
+        if (budgetVal > 20000) {
+            alert("The Budget value cannot exceed £20000");
+        } else if (budgetVal < totalExpenses) {
+            alert("The Budget value cannot be lower that the spent amout £" + totalExpenses);
+        } else {
+            setBudget(event.target.value);
+        }
+    }
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span className='flex'>
+                Budget: £<input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    step='10'
+                    value={budgetAmt}
+                    style={{ marginLeft: '2rem' , size: 10}}
+                    onChange={(event) => OnBudgetChange(event)}>
+                </input>
+            </span>
         </div>
     );
 };
